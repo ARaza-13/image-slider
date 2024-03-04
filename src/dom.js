d9c1da72a6fd5ccb2faac38a79b19fead2735512
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-03-04 14:52:22 -0500

    Made the first image of the image array set as active while the rest are hidden

--- src/dom.js
+++ src/dom.js
@@ -5,11 +5,17 @@ export default class DOMManager {
   static initialize() {
     CreateHtml.initializeHtml();
     DOMManager.loadImages();
+    DOMManager.loadFirstImage();
   }
 
   static loadImages() {
     imageFolder.forEach((image) => {
       CreateHtml.createImage(image);
     });
+  }
+
+  static loadFirstImage() {
+    const img = document.querySelector('.image');
+    img.classList.add('active');
   }
 }
